d4a9a914e9e43d6176f326c0500ae649f16050e2
ashfaq <ashfaq.m@techsophy.com>
2018-01-18 19:48:57 +0530

    optimized the code. now all the fuctions are working.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 // import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
-import DocumentVerification from '../build/contracts/ProofOfExistence4.json'
+import DocumentVerification from '../build/contracts/DocumentVerification.json'
 import getWeb3 from './utils/getWeb3'
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
@@ -18,11 +18,12 @@ class App extends Component {
     this.state = {
       truffleInstance: null,
       web3: null,
-      verified: false,
-      checked: false,
+      verify: false,
+      check: false,
       verify_text:'',
       check_text:'',
       accounts: null,
+      already_verified: false,
     }
   }
 
@@ -35,71 +36,126 @@ class App extends Component {
         web3: results.web3
       })
       // Instantiate contract once web3 provided.
-      this.instantiateContract(this.updateTruffleInstance.bind(this))
+      this.instantiateContract(this.updateState.bind(this))
     })
     .catch(() => {
       console.log('Error finding web3.')
     })
   }
 
-  instantiateContract(updateTruffleInstance) {
+  instantiateContract(setTruffleInstanceAndAccounts) {
     const contract = require('truffle-contract')
     const documentVerification = contract(DocumentVerification)
     documentVerification.setProvider(this.state.web3.currentProvider);
-    var documentVerificationInstance;
+
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
       documentVerification.deployed().then((instance) => {
-        documentVerificationInstance = instance;
-        updateTruffleInstance(instance, accounts);
-        // Validate document.
-        var response = documentVerificationInstance.notarize('s2addsd', {from: accounts[0]})
-                        .then((resp)=>{
-                          console.log("response of verify Document call", resp);
-                        })
-        return response
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        var test = documentVerificationInstance.checkDocument('s3adsd').then((result)=>{
-            console.log("response", result);
-          })
-        return test
+        setTruffleInstanceAndAccounts({
+          truffleInstance: instance,
+          accounts: accounts
+        })
       }).catch((error)=>{
-        console.log("ERROR", error);
+        console.log("ERROR ", error);
       })
     })
   }
 
-  updateTruffleInstance(truffleInstance, accounts){
-    this.setState({
-      truffleInstance: truffleInstance,
-      accounts: accounts
-    });
+  updateState(data: Object){
+    this.setState(data);
   }
 
-  checkDocument(){
-    this.state.truffleInstance.checkDocument(this.state.check_text).then((response)=>{
-      console.log("resdponse", response);
+
+  handleDocumentVerification(){
+    this.verifyDocument().then((response)=>{
+      if(response.tx){
+        this.setState({
+          verify: true
+        })
+      }
     })
   }
 
   verifyDocument(){
-    this.state.truffleInstance.notarize(this.state.verify_text,{from: this.state.accounts[0]}).then((response)=>{
-      console.log("resdponse", response);
+    return this.state.truffleInstance
+      .verifyDocument(this.state.verify_text, { from: this.state.accounts[0] })
+      .then(response => {
+        return response
+      }).catch((err)=>{
+        console.log("Error while verifing Document");
+      })
+  }
+
+  handleDocumentChecking(){
+    this.checkDocument(this.updateState.bind(this)).then((response)=>{
+      if(response){
+        this.setState({
+          already_verified: response,
+          check: true
+        })
+      }
     })
   }
 
+  checkDocument(updateState){
+    return this.state.truffleInstance
+      .checkDocument(this.state.check_text)
+      .then((response) => {
+        updateState({
+          already_verified:response,
+          check: true
+        })
+        return response
+      })
+      .catch(err => {
+        console.log("Error while checking document");
+      });
+  }
+
   handleChange(event){
-    this.setState({[event.target.name]: event.target.value})
+    var message_state =  event.target.name.substring(0, event.target.name.length-5)
+    this.setState({
+      [event.target.name]: event.target.value,
+      [message_state]: false
+    })
   }
 
   renderResponseMessage(name){
-    if(this.state[name] && this.state[event.target.name]){
-      return null
+    var message;
+    if(name === 'verify'){
+      message = this.state.verify
+                  ? "The Document Verified Successfully."
+                  : "Unable to verify the Document.";
+    }else{
+      message = this.state.already_verified
+                 ? "The Document is verified."
+                 : "The Document need to be verified.";
+    }
+
+    if(this.state[name]){
+      return(
+        <div className="pure-u-1-1" style={{marginTop: 30}}>
+          <h2 style={{marginLeft:10}}> {message} </h2>
+        </div>
+      )
     } else{
-      console.log("SDSD")
       return null
     }
+  }
+
+  renderTextInputAndButton(data: Object){
+    return(
+      <div className="pure-u-1-1" style={{marginTop: 30}}>
+        <TextField
+          name={data.name}
+          floatingLabelText={data.text_input_label}
+          floatingLabelStyle={{color: orange500}}
+          floatingLabelFocusStyle={{color: blue500}}
+          onChange={this.handleChange.bind(this)}
+        />
+        <RaisedButton label={data.button_label} style={{marginLeft:10}} onClick={this[data.onButtonClick].bind(this)} />
+      </div>
+    )
   }
 
   render() {
@@ -111,28 +167,18 @@ class App extends Component {
           </nav>
           <main className="container">
             <div className="pure-g row">
-              <div className="pure-u-1-1" style={{marginTop: 30}}>
-                <TextField
-                  name="verify_text"
-                  floatingLabelText="Verify Document"
-                  floatingLabelStyle={{color: orange500}}
-                  floatingLabelFocusStyle={{color:blue500}}
-                  onChange={this.handleChange.bind(this)}
-                />
-                <RaisedButton label="Verify" style={{marginLeft:10}} onClick={this.verifyDocument.bind(this)} />
-              </div>
-              {this.renderResponseMessage('verified')}
-              <div className="pure-u-1-1" style={{marginTop: 30}}>
-                <TextField
-                  name='check_text'
-                  floatingLabelText="Check Verified Document"
-                  floatingLabelStyle={{color: orange500}}
-                  floatingLabelFocusStyle={{color:blue500}}
-                  onChange={this.handleChange.bind(this)}
-                />
-                <RaisedButton label="Check" style={{margin: 5}} onClick={this.checkDocument.bind(this)} />
-              </div>
-              {this.renderResponseMessage('checked')}
+              {this.renderTextInputAndButton({
+                name:'verify_text',
+                text_input_label:'Verify Document',
+                button_label:'Verify',
+                onButtonClick:'handleDocumentVerification'})}
+              {this.renderResponseMessage('verify')}
+              {this.renderTextInputAndButton({
+                name:'check_text',
+                text_input_label:'Check Verified Document',
+                button_label:'Check',
+                onButtonClick:'handleDocumentChecking'})}
+              {this.renderResponseMessage('check')}
             </div>
           </main>
         </div>
